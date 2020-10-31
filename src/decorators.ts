efb889d580abd46472e439f03f09c729e599fc77
a person <suyashtnt@gmail.com>
2020-10-31 21:32:45 +0200

    update naming schem

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,26 +1,26 @@
-import { app, router } from '.';
+import { _app, _router } from '.';
 
 export function get(location: string) {
 	return (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) => {
-		router.get(location, descriptor.value);
+		_router.get(location, descriptor.value);
 	};
 }
 
 export function post(location: string) {
 	return (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) => {
-		router.post(location, descriptor.value);
+		_router.post(location, descriptor.value);
 	};
 }
 
 export function put(location: string) {
 	return (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) => {
-		router.put(location, descriptor.value);
+		_router.put(location, descriptor.value);
 	};
 }
 
 export function remove(location: string) {
 	return (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) => {
-		router.delete(location, descriptor.value);
+		_router.delete(location, descriptor.value);
 	};
 }
 
@@ -33,10 +33,10 @@ export function use(...paths: string[]) {
 		if (paths[0]) {
 			for (let i = 0; i < paths.length; i++) {
 				const path = paths[i];
-				app.use(path, descriptor.value);
+				_app.use(path, descriptor.value);
 			}
 		} else {
-			app.use(descriptor.value);
+			_app.use(descriptor.value);
 		}
 	};
 }
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import express, { Express } from 'express';
 /**
  * DO NOT USE. ONLY USED FOR INTERNALS
  */
-export let app = express();
+export let _app = express();
 
 /**
  * DO NOT USE. ONLY USED FOR INTERNALS
  */
-export const router = express.Router({
+export const _router = express.Router({
 	strict: true,
 });
 
@@ -18,8 +18,8 @@ export const router = express.Router({
  * @param cb the function to all after it starts
  */
 export function start(PORT: number, cb: () => void) {
-	app.use(router);
-	app.listen(PORT, cb);
+	_app.use(_router);
+	_app.listen(PORT, cb);
 }
 
 /**
@@ -28,11 +28,11 @@ export function start(PORT: number, cb: () => void) {
  * @param val the value
  */
 export function set(setting: string, val: any) {
-	app.set(setting, val);
+	_app.set(setting, val);
 }
 
 export function setApp(appToUse: Express) {
-	app = appToUse;
+	_app = appToUse;
 }
 
 export * from './decorators';
